e74a0aa702c694860b5265eae58e8f84d5cb4c69
Robert Strandh <robert.strandh@gmail.com>
2023-01-14 10:24:37 +0100

    Define application frame and main entry point.

--- Code/GUI/gui.lisp
+++ Code/GUI/gui.lisp
@@ -1,3 +1,29 @@
 (cl:in-package #:clovetree-gui)
 
-(defun clovetree ())
+(clim:define-application-frame clovetree ()
+  ()
+  (:panes
+   (score :application
+          :width 1000 :height 1000
+          :display-time nil)
+   (interactor :interactor
+               :width 1000 :height 100))
+  (:layouts
+   (default (clim:vertically ()
+              (clim:scrolling (:width 1000 :height 1000)
+                score)
+              (clim:scrolling (:width 1000 :height 100)
+                interactor)))))
+
+(defun clovetree (&key new-process (process-name "Clovetree"))
+  (with-open-file (stream
+                   (uiop:xdg-config-home "clovetree" "init.lisp")
+                   :if-does-not-exist nil)
+    (unless (null stream)
+      (load stream)))
+  (let ((frame (clim:make-application-frame
+                'clovetree)))
+    (flet ((run () (clim:run-frame-top-level frame)))
+      (if new-process
+          (clim-sys:make-process #'run :name process-name)
+          (run)))))
