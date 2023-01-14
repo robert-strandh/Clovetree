b30ad5faec96b2ba5594df5279dbba1bb9af5b96
Robert Strandh <robert.strandh@gmail.com>
2023-01-14 15:46:24 +0100

    Define embryonic command LOAD-SCORE-FILE.

--- Code/GUI/commands.lisp
+++ Code/GUI/commands.lisp
@@ -2,3 +2,9 @@
 
 (define-clovetree-command (com-quit :name t) ()
   (clim:frame-exit clim:*application-frame*))
+
+(define-clovetree-command (com-load-score-file :name t)
+    ((pathname 'pathname
+               :default *default-pathname-defaults*
+               :insert-default t))
+  (declare (ignore pathname)))
