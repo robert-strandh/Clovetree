e0c55411898a4d85b4bbeb31a8bac52fb91f15fb
Robert Strandh <robert.strandh@gmail.com>
2023-01-15 13:39:47 +0100

    Add explicit DEFGENERIC forms for slot accessors.

--- Code/Model/score.lisp
+++ Code/Model/score.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:clovetree-model)
 
+(defgeneric title (score))
+
+(defgeneric (setf title) (new-title score))
+
+(defgeneric parts (score))
+
+(defgeneric (setf parts) (new-parts score))
+
 (defclass score ()
   ((%title
     :initarg :title
