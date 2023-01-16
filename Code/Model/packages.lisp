51070756bd1023a25444d6a32021b262ff505d41
Robert Strandh <robert.strandh@gmail.com>
2023-01-16 11:36:27 +0100

    Export names of classes and accessors.

--- Code/Model/packages.lisp
+++ Code/Model/packages.lisp
@@ -3,4 +3,17 @@
 (defpackage #:clovetree-model
   (:use #:common-lisp)
   (:shadow #:position)
-  (:export))
+  (:export
+   #:score #:parts #:title
+   #:measure-separator #:strength
+   #:key-signature #:accidental-count
+   #:time-signature
+   #:numeric-time-signature
+   #:common-time-signature
+   #:cut-common-time-signature
+   #:beat-count #:beat-type
+   #:clef #:g-clef #:f-clef #:c-clef
+   #:size #:position
+   #:temporal-element #:duration
+   #:cluster #:notes
+   #:part #:elements))
