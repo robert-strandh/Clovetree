76067d1dc1689d00a7055cb5d960644ecc5f39cd
Robert Strandh <robert.strandh@gmail.com>
2023-01-18 16:26:48 +0100

    Use POSITIONAL-ELEMENT as a superclass of REST.

--- Code/Model/rest.lisp
+++ Code/Model/rest.lisp
@@ -1,11 +1,4 @@
 (cl:in-package #:clovetree-model)
 
-;;; The position of the rest can be NIL or an integer representing the
-;;; staff position.  If it is NIL, that means the default position for
-;;; the rest with the duration indicated by a slot in the superclass.
-
-(defclass rest (temporal-element)
-  ((%position
-    :initform nil
-    :initarg :position
-    :accessor position)))
+(defclass rest (temporal-element positional-element)
+  ())
