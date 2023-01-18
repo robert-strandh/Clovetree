01de6543c1bbe6521c31e2861d1afa54f8c64c48
Robert Strandh <robert.strandh@gmail.com>
2023-01-18 17:31:39 +0100

    Add slot BEAMS containing a list of instances of class BEAM.

--- Code/Model/cluster.lisp
+++ Code/Model/cluster.lisp
@@ -1,14 +1,26 @@
 (cl:in-package #:clovetree-model)
 
-;;; A cluster represents a collection of notes plus annotations such
-;;; as articulations that are common for all notes in a cluster.
+;;; A cluster represents a collection of noteheads plus annotations
+;;; such as articulations that are common for all noteheads in a
+;;; cluster.
 
 (defgeneric noteheads (cluster))
 
 (defgeneric (setf noteheads) (new-noteheads cluster))
 
+(defgeneric beams (cluster))
+
+(defgeneric (setf beams) (new-beams cluster))
+
 (defclass cluster (temporal-element)
   ((%noteheads
     :initform '()
     :initarg :noteheads
-    :accessor noteheads)))
+    :accessor noteheads)
+   ;; This slot contains a list of instances of the class BEAM.  The
+   ;; beams in the list are ordered from the one furthest from the
+   ;; noteheads to the one closest to the noteheads.
+   (%beams
+    :initform '()
+    :initarg :beams
+    :accessor beams)))
