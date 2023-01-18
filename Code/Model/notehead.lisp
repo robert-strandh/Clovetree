fe1e11f9602f3dbd49ec934c921aa056438b7e2e
Robert Strandh <robert.strandh@gmail.com>
2023-01-18 16:47:04 +0100

    Add a slot ACCIDENTAL to class NOTEHEAD.

--- Code/Model/notehead.lisp
+++ Code/Model/notehead.lisp
@@ -1,7 +1,17 @@
 (cl:in-package #:cluster-model)
 
+;;; The accidental of a notehead can be NIL or an instance of the class
+;;; ACCIDENTAL.
+
+(defgeneric accidental (notehead))
+
+(defgeneric (setf accidental) (new-accidental notehead))
+
 (defclass notehead (positional-element)
-  ())
+  ((%accidental
+    :initform nil
+    :initarg :accidental
+    :accessor accidental)))
 
 (defclass whole-notehead (notehead)
   ())
