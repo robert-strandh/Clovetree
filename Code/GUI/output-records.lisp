7d0ea15c6aef1e42a3ac838fde4841f53ba2c5d4
Robert Strandh <robert.strandh@gmail.com>
2023-02-28 15:30:42 +0100

    Define more record types.

--- Code/GUI/output-records.lisp
+++ Code/GUI/output-records.lisp
@@ -1,1 +1,38 @@
 (cl:in-package #:clovetree-gui)
+
+(defclass record (clim:output-record)
+  ((%x-position :initarg :x-position :reader x-position)
+   (%parent :initarg :parent :reader parent)))
+
+(defclass compound-record (record)
+  ((%contents :initarg :contents :accessor contents)))
+
+(defclass displayed-record (record clim:displayed-output-record)
+  ())
+
+(defclass notehead-record (displayed-record)
+  ())
+
+(defclass stem-record (displayed-record)
+  ())
+
+(defclass rest-record (displayed-record)
+  ())
+
+(defclass accidental-record (displayed-record)
+  ())
+
+(defclass flag-record (displayed-record)
+  ())
+
+(defclass beam-record (displayed-record)
+  ())
+
+(defclass cluster-record (compound-record)
+  ())
+
+(defclass part-record (compound-record)
+  ())
+
+(defclass system-record (compound-record)
+  ())
